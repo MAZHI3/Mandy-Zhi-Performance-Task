7c6583383d124400d9b68d83865bc32bb343c59e
MAZHI3 <122053601+MAZHI3@users.noreply.github.com>
2023-12-12 12:00:01 -0800

    Player Scene completed

---
+++ scripts/player.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+@export var speed = 400
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("move-up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move-down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move-left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move-right"):
+		velocity.x += 1
+		
+	velocity = velocity.normalized() * speed
+		
+	position += velocity * delta
+	
